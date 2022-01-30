51f3998d86d847a9ed2fa6d1ef6a57424cf5878b
Andrey Zavrichko <andrey@zavrichko.dev>
2022-01-30 14:36:13 +0200

    create start project

---
+++ src/test/java/zavrichko/tests/TestBase.java
@@ -0,0 +1,42 @@
+package zavrichko.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import zavrichko.helpers.Attach;
+
+import java.net.MalformedURLException;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class TestBase {
+    @BeforeAll
+    static void setUp() throws MalformedURLException {
+        Configuration.remote = "http://149.154.70.38:4444/wd/hub";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        ChromeOptions options = new ChromeOptions();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "chrome");
+        capabilities.setCapability("browserVersion", "95.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+
+    }
+
+   @AfterEach
+   void addAttachments() {
+       Attach.screenshotAs("Last screenshot");
+       Attach.pageSource();
+       Attach.browserConsoleLogs();
+       Attach.addVideo();
+       closeWebDriver();
+    }
+}
